e2f7be18e806b57c86de4c52b5188077705e9d48
Michael Trage <osuperyuu@gmail.com>
2022-06-20 20:01:06 -0300

    Continuado a refazer as validacoes Nome Aula: Domain Validation: NotNullOrEmpty

--- src/FC.Codefix.Catalog.Domain/Validation/DomainValidation.cs
+++ src/FC.Codefix.Catalog.Domain/Validation/DomainValidation.cs
@@ -4,11 +4,19 @@ namespace FC.Codefix.Catalog.Domain.Validation
 {
     public class DomainValidation
     {
-        public static void NotNull(object target, string fieldName)
+        public static void NotNull(object? target, string fieldName)
         {
             if (target is null)
             {
                 throw new EntityValidationException($"{fieldName} should not be null!");
+            }
+        }
+
+        public static void NotNullOrEmpty(string? target, string fieldName)
+        {
+            if (String.IsNullOrWhiteSpace(target))
+            {
+                throw new EntityValidationException($"{fieldName} should not be null or empty!");
             }
         }
     }
